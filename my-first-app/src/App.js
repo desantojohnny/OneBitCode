e6a63ba309a677c0dff3c6619b347b72a7d49c1d
desantojohnny <johnny.santo@gmail.com>
2022-02-23 20:20:24 -0300

    miniapp com boolean

---
+++ my-first-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function HelloWorld(){
+  return <h1>Hello World</h1>;
+}
+
+function App() {
+  return (
+    <div>
+      <HelloWorld/>
+    </div>
+  );
+}
+
+export default App;
