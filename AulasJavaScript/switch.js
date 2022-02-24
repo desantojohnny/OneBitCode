b79de171220c75e90c86fc52aec7bbe3c6a0b77f
desantojohnny <johnny.santo@gmail.com>
2022-02-24 08:43:13 -0300

    aula 23 de fev de 2022

---
+++ AulasJavaScript/switch.js
@@ -0,0 +1,29 @@
+/*let velocity = 80 // variável recebento uma velocidade
+
+switch(velocity * 2){ //calculo da velocidade e conforme o resultado ele irá cair na condição abaixo
+    case 60:
+    case 80: // ele vai percorrer todos os case eo primeiro que atender a condição ele para(break)
+    case 100:
+    console.log('Sua velocidade é 100')
+    break //ele sempre para no break que atende a condição
+    case 160:
+    console.log('Sua velocidade é 160')
+    break
+    default:
+        console.log('Velocidade não identificada')
+}
+*/
+let time = "Flamento"
+switch(time){
+    case "Flamengo":
+        console.log("Super Campeão em 2019")
+        break
+        case  "Ponte Preta":
+        console.log("Não ganho nenhum título ainda")
+        break
+        case "Atletico MG":
+            console.log("Chorão")
+        break
+        default:
+            console.log("Time não encontrado")
+}
